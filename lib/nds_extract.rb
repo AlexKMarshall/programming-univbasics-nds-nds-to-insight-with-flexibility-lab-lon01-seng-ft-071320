53a2f9be584518d17dc624e20c9f7047a84d277e
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-22 14:30:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,6 +43,10 @@ end
 
 
 def gross_per_studio(collection)
+  result = {}
+  
+  collection.count.times do |index|
+  end
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
