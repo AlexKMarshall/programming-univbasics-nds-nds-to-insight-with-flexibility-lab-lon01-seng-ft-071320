4c182b2ca97dd541ab907378b64f71fe45f7dd7c
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-22 14:29:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,20 +34,11 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  result = []
+  movies_collection.count.times do |index|
+    result << movie_with_director_name(name, movies_collection[index])
+  end
+  result
 end
 
 
@@ -66,16 +57,13 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  result = []
+  source.count.times do |index|
+    director = source[index][:name]
+    movies = source[index][:movies]
+    result << movies_with_director_key(director, movies)
+  end
+  result
 end
 
 # ----------------    End of Your Code Region --------------------
